479da4cff6436d101458c80518c574afc9c52bf3
kallyas <kallyasmedia@gmail.com>
2025-05-15 20:05:21 +0300

    chore: Standardize import and JSX formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider } from "notistack";
 import App from "./App";
 import { theme, darkTheme } from "./styles/theme";
 import { SearchHistoryProvider } from "./contexts/SearchHistoryContext";
 import { ThemeModeProvider } from "./contexts/ThemeContext";
 import "./styles/global.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeModeProvider>
       {(mode) => (
-        <ThemeProvider theme={mode === 'dark' ? darkTheme : theme}>
+        <ThemeProvider theme={mode === "dark" ? darkTheme : theme}>
           <CssBaseline />
-          <SnackbarProvider 
-            maxSnack={3} 
+          <SnackbarProvider
+            maxSnack={3}
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
+              vertical: "bottom",
+              horizontal: "right",
             }}
           >
             <SearchHistoryProvider>
               <Router>
                 <App />
-                <Analytics />
               </Router>
+              <Analytics />
             </SearchHistoryProvider>
           </SnackbarProvider>
         </ThemeProvider>
